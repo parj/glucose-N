5aee9cc71109be9398970d1a44acf0266cc907f8
Parjanya Mudunuri <parjanya@gmail.com>
2012-08-17 13:45:51 +0530

    added sanity checks and prevented refresh of page on form submit

--- public/javascripts/connection.js
+++ public/javascripts/connection.js
@@ -2,11 +2,45 @@ var hostname = 'http://' + document.location.hostname;
 var socket = io.connect(hostname);
 
 function sendMessage() {
-    socket.send(document.getElementById('txtCommand').value);
+    var boolOK = sanityCheck();
+    if (boolOK)
+        socket.send(document.getElementById('txtCommand').value);
+}
+
+function sanityCheck() {
+    var command = document.getElementById('txtCommand').value;
+    var boolOK = true;
+    var error = "";
+
+    if (command.indexOf("@") == -1) {
+        boolOK = false;
+        error = " Missing @,";
+    } if (command.indexOf("^") == -1) {
+        boolOK = false;
+        error += " Missing ^,";
+    } if (command.indexOf("@") == 0) {  //No text before @
+        boolOK = false;
+        error += " No text before @,";
+    } if (command.indexOf("^") == 0) {  //No text before @
+        boolOK = false;
+        error += " No text before ^,";
+    }
+
+    if (!boolOK) {
+        document.getElementById('divError').setAttribute("class", "control-group error");
+        document.getElementById('btnCommand').setAttribute("class", "btn btn-danger");
+        document.getElementById('txtHelp').textContent = error;
+
+    } else {
+        document.getElementById('divError').setAttribute("class","control-group");
+        document.getElementById('btnCommand').setAttribute("class", "btn btn-primary");
+        document.getElementById('txtHelp').textContent = "";
+    }
+
+    return boolOK;
 }
 
 socket.on('message', function(data){
     document.getElementById('txtResult').value += data;
-    document.getElementById('txtResult').value += "----------\n";
+    document.getElementById('txtResult').value += "\n-----EOL-----\n\n";
 });
-
